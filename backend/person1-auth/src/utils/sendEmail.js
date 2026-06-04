d8fc6683bd5c561ef1f02ba5cddb5f408fba389c
Devansh Garg <degarg3245@gmail.com>
2026-06-04 20:50:43 +0530

    fix: force IPv4 for Nodemailer to resolve Render ENETUNREACH

--- backend/person1-auth/src/utils/sendEmail.js
+++ backend/person1-auth/src/utils/sendEmail.js
@@ -1,9 +1,13 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
 
 const sendEmail = async (to, subject, text) => {
 
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
